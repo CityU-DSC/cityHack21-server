8867d9aafb8b5ea28d92f282216f0e2d1879cc32
lowzhao <lowzhao@gmail.com>
2020-12-23 23:14:36 +0800

    feature requestMiddle function

--- controller/userController.js
+++ controller/userController.js
@@ -72,6 +72,7 @@ exports.loginUser = async (req, res) =>
         res.status(400).json({ err: err });
     }
 };
+
 exports.getUserDetails = async (req, res) =>
 {
     const user = await User.findById(req.userData._id);
@@ -217,3 +218,16 @@ exports.isAWSVerified = async (req, res) => {
         });
     }
 }
+
+exports.emailUsed = async (req) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    return { emailUsed: !!user };
+}
+
+exports.accountIdUsed = async (req) => {
+    const { accountId } = req.body;
+    const user = await User.findOne({ accountId });
+    return { accountIdUsed: !!user };
+}
+
